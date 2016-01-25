9311b344b633883befcf515a0eec84926b922432
Carl Bolstad <cbolstad@gmail.com>
2016-01-25 14:46:33 -0800

    missing part of basic rendering of coach availability on calendar

--- src/components/coaches.jsx
+++ src/components/coaches.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
-export default React.createClass({
-	handleClick: function () {
-		//$('#calendar').fullCalendar();
-	},
-	render: function() {
-		return <ul>
-			{this.props.coaches.map(coach => 
-				<li key={coach} onClick={this.handleClick}>{coach}</li>
-			)}
-		</ul>;
+ class CoachList extends React.Component {
+	// constructor(props){
+	// 	super(props);
+	// }
+	render() {
+		return (<ul>
+			{this.props.coaches.map(coach => <CoachRow key={coach.name} coach={coach}/> )}
+		</ul>);
 	}
-});
+}
+
+class CoachRow extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick() {
+		const {availability} = this.props.coach;
+
+		$('#calendar').fullCalendar('removeEvents');
+		$('#calendar').fullCalendar('addEventSource', availability);
+		$('#calendar').fullCalendar('rerenderEvents');
+	}
+	render() {
+		return <li onClick={this.handleClick} >{this.props.coach.name}</li>
+	}
+}
+
+ export default CoachList;
