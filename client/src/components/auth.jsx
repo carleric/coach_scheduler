df5130f49252e446ee6110db47aba469289d0dde
Carl Bolstad <cbolstad@gmail.com>
2016-02-02 23:29:21 -0800

    prettier login form

--- client/src/components/auth.jsx
+++ client/src/components/auth.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import auth from '../auth.js';
+import { Link } from 'react-router';
 
 class Login extends React.Component{
   constructor(props, context) {
@@ -36,16 +37,51 @@ class Login extends React.Component{
   render() {
     return (
       <div className="ui segments">
+    
+
         <div className="ui segment">
-          <form onSubmit={this.handleSubmit}>
-            <label><input ref="email" placeholder="email" defaultValue="Joe" /></label>
-            <label><input ref="pass" placeholder="password" /></label> (hint: joe)<br />
-            <button type="submit">login</button>
-            {this.state.error && (
-              <p>Bad login information</p>
-            )}
-          </form>
+
+          <div className="ui middle aligned center aligned grid">
+            <div className="column">
+              <h2 className="ui teal image header">
+                
+                <div className="content">
+                  Log-in to your account
+                </div>
+              </h2>
+              <form className="ui large form" onSubmit={this.handleSubmit}>
+                <div className="ui stacked segment">
+                  <div className="field">
+                    <div className="ui left icon input">
+                      <i className="user icon"></i>
+                      <input ref="email" type="text" name="email" placeholder="E-mail address"/>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="ui left icon input">
+                      <i className="lock icon"></i>
+                      <input ref="pass" type="password" name="password" placeholder="Password"/>
+                    </div>
+                  </div>
+            
+                  <button className="ui fluid large teal submit button" type="submit">login</button>
+                </div>
+
+                <div className="ui error message"></div>
+
+              </form>
+
+              {/*<div className="ui message">
+                New to us? <Link to="/signup">Sign Up</Link>
+              </div>*/}
+            </div>
+          </div>
+
+
         </div>
+
+
+
       </div>
     )
   }
